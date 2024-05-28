d21f08ff67432329dd9f462bb4092ba82ca02dd3
Indoradesu <indrastyawan0925@gmail.com>
2024-05-29 03:39:41 +0700

    fix: add transformation page

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,10 +1,30 @@
+import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/TransformationForm'
+import { transformationTypes } from '@/constant'
+import { getUserById } from '@/lib/actions/user.actions'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const AddTransformationPage = () => {
+const AddTransformationPage = async ({ params:{type} }: SearchParamProps) => {
+  const {userId} = auth()
+  const transformation = transformationTypes[type]
+  
+  if(!userId) redirect('/sign-in')
+
+  const user = await getUserById(userId)
   return (
-    <div>
-      
-    </div>
+   <>
+     <Header 
+    title={transformation.title} 
+    subTitle={transformation.subTitle}
+    />
+    <TransformationForm 
+    action='Add'
+    userId={user._id}
+    type={transformation.type as TransformationTypeKey}
+    creditBalance={user.creditBalance}/>
+   </>
   )
 }
 
